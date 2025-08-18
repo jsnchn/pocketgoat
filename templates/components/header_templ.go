fa222145aecab0cd281d5ca2971265ce636bd3dc
Jason Chen <jchen.json@gmail.com>
2025-08-18 11:44:56 -0700

    restyled everything

--- templates/components/header_templ.go
+++ templates/components/header_templ.go
@@ -8,8 +8,8 @@ package components
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-// Brand component that displays the site logo/name
-func Brand() templ.Component {
+// Header component that combines brand and user menu in a navbar
+func Header() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
 		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
@@ -30,7 +30,15 @@ func Brand() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div><a href=\"/\"><img src=\"/static/images/logo/32x32.png\" alt=\"PocketGoat\" width=\"32\" height=\"32\"> <span>PocketGoat</span></a></div>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div class=\"navbar\"><div class=\"flex-1\"><a href=\"/\" class=\"flex flex-row gap-1 font-bold text-xl\"><img src=\"/static/images/logo/32x32.png\" alt=\"PocketGoat\" width=\"32\" height=\"32\"> PocketGoat</a></div><div class=\"flex-none\">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = AuthMenu().Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "</div></div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
--- templates/layouts/base_layout_templ.go
+++ templates/layouts/base_layout_templ.go
@@ -59,11 +59,7 @@ func BaseLayout(title string, extraScripts ...string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = components.Brand().Render(ctx, templ_7745c5c3_Buffer)
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		templ_7745c5c3_Err = components.UserMenu().Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = components.Header().Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
